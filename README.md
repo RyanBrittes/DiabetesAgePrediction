d48fb9f7dc2ffa8374aea08641fbe8552de5dadf
ryanbrittes <ryanbrittes027@gmail.com>
2025-07-20 16:42:41 -0400

    update

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# DiabetesAgePrediction
-Repository used to predict how old a person can get diabetes based in markers using a Linear Regression
+# Diabetes Age Prediction - Linear Regression:
+Algoritmo de Machine Learning utilizado para prever a idade que mulheres acima dos 21 anos provenientes da India podem desenvolver diabetes baseado em seus biomarcadores
+
+[![Status](https://img.shields.io/badge/Status-Em%20Desenvolvimento-yellow)]()
+[![Python](https://img.shields.io/badge/Python-3.13-blue?logo=python)]()
+[![Pandas](https://img.shields.io/badge/Pandas-blue?logo=pandas)]()
+[![Numpy](https://img.shields.io/badge/Numpy-lightgrey?logo=numpy)]()
+[![LinearRegression](https://img.shields.io/badge/LinearRegression-purple)]()
+[![Feito por Ryan](https://img.shields.io/badge/Feito%20por-Ryan%20Brittes-blue?logo=github)]()
+---
+
+## :beginner: Tecnologias utilizadas:
+- **Pandas**
+- **Numpy**
+- **Matplotlib**
+
+## :pencil: Explicação
+Com este repositório você poderá entender um pouco melhor como a técnica de Regressão Linear utilizada em Machine Learning funciona e pode ser aplicada aos problemas reais do dia a dia. Como exemplo foi utilizada uma base de dados pré-existente e real.
+
+O intuito da análise realizada foi tentar prever com que idade uma pessoa pode desenvolver diabetes baseada em seus biomarcadores, este estudo foi dirigido na India com mulheres acima de 21 anos.A fim de credibilizar os pesquisadores deixarei ao final da explicação o link para acesso ao arquivo original.
+
+Caso você já tenha algum conhecimento prévio do que se trata o conceito base, sugiro que pule para a etapa de explicação do código e dos dados encontrados.
+
+
+### ***Regressão Linear:***
+> É uma técnica estatística utilizada para encontrar a relação entre variáveis, em machine learning é possível encontrar a relação entre atributos e rótulos de um conjunto de dados.
--- linearRegression.py
+++ linearRegression.py
@@ -11,23 +11,50 @@ class LinearRegression():
         self.weight = np.zeros(self.xTrue.shape[1])
         self.bias = 0
         self.lr = 0.00001
-        self.epochs = 4000
+        self.epochs = 10000000
+        self.batchSize = 100
         self.n = len(self.yTrue)
         self.losses = []
-        self.batchSize = 300
+        self.rateTest = 0.1
 
     def funcMSE(self, yTrue, yPred):
+        return np.mean((yTrue - yPred) ** 2)
+    
+    def funcMSA(self, yTrue, yPred):
         return np.mean(yTrue - yPred)
+    
+    def shuffleData(self):
+        np.random.seed(42)
+        indexShuffled = np.random.permutation(self.n)
+
+        yShuffled = self.yTrue[indexShuffled]
+        xShuffled = self.xTrue[indexShuffled]
+        
+        listShuffled = [xShuffled, yShuffled]
+        
+        return listShuffled
+
+    def separeTrainTest(self, xValues, yValues):
+        rateTrain = 1 - self.rateTest
+        lenTrain = np.floor(len(xValues)*rateTrain).astype(int)
+
+        xTrain = xValues[0:lenTrain]
+        yTrain = yValues[0:lenTrain]
+        xTest = xValues[lenTrain + 1:len(xValues)]
+        yTest = yValues[lenTrain + 1:len(xValues)]
+
+        listSepared = [xTrain, xTest, yTrain, yTest]
+
+        return listSepared
 
     def trainModel(self):
+        xTrain, xTest, yTrain, yTest = self.separeTrainTest(self.shuffleData()[0], self.shuffleData()[1])
+        
         for epoch in range(self.epochs):
-            indexShuffled = np.random.permutation(self.n)
-            yShuffled = self.yTrue[indexShuffled]
-            xShuffled = self.xTrue[indexShuffled]
 
             for i in range(0, self.n, self.batchSize):
-                xBatch = xShuffled[i:i+self.batchSize]
-                yBatch = yShuffled[i:i+self.batchSize]
+                xBatch = xTrain[i:i+self.batchSize]
+                yBatch = yTrain[i:i+self.batchSize]
 
                 yPred = xBatch @ self.weight + self.bias
                 error = yPred - yBatch
@@ -41,19 +68,36 @@ class LinearRegression():
             epoch_pred = self.xTrue @ self.weight + self.bias
             lossValue = self.funcMSE(self.yTrue, epoch_pred)
             self.losses.append(lossValue)
-            #print(f"Loss --> {lossValue:4f}")
+            print(f"Loss --> {lossValue:4f}")
+            
+        trainedParams = [self.weight, self.bias, self.losses, xTrain, xTest, yTrain, yTest]
 
-        print(f"Pesos encontrados: {self.weight}\nVies encontrado: {self.bias}\nPerca final: {lossValue}")
-        xTest = [5, 165, 70, 35, 210, 35.6]
-        yTest = xTest @ self.weight + self.bias
-        print(f"\n\nTeste com valores reais:\nPregnancies: {xTest[0]}\nGlucose: {xTest[1]}\nBloodPressure: {xTest[2]}\nSkinThickness: {xTest[3]}\nInsulin: {xTest[4]}\nBMI: {xTest[5]}\nIdade de previsão para aderir a doença: {yTest}")
+        return trainedParams
+    
+    def showResults(self):
+        results = self.trainModel()
+
+        xTest = results[4]
+        yTest = results[6]
+        trainedWeight = results[0]
+        trainedBias = results[1]
+        finalLoss = results[2]
+
+        for i in range(len(xTest)):
+            yPredict = xTest[i] @ trainedWeight + trainedBias
+            print("--------------------------------------")
+            print(f"Pregnancies: {xTest[i][0]}\nGlucose: {xTest[i][1]}\nBloodPressure: {xTest[i][2]}\nSkinThickness: {xTest[i][3]}\nInsulin: {xTest[i][4]}\nBMI: {xTest[i][5]}")
+            print(f"Previsão: {yPredict} | Real: {yTest[i]}")
+        
+        print(f"Pesos encontrados: {trainedWeight}\nVies encontrado: {trainedBias}\nPerca final: {finalLoss[len(finalLoss) - 1]:4f}")
+
 
     def plotLoss(self):
-        self.trainModel()
+        self.showResults()
         plt.plot(self.losses)
         plt.xlabel("Época")
         plt.ylabel("Erro (MSE)")
-        plt.title("Convergência da Regressão Linear Múltipla")
+        plt.title("Perca por período de treinamento")
         plt.grid()
         plt.show()
     
